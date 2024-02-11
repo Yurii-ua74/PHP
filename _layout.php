d09bdfa8bb9823f97ead61be6715a3d06d91dc2b
Yurii-ua74 <postffild@gmail.com>
2024-02-11 13:37:18 +0200

    bd

--- _layout.php
+++ _layout.php
@@ -8,28 +8,31 @@
     <!--Let browser know website is optimized for mobile-->
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>PHP</title>
+    <link rel="stylesheet" href="/css/site.css"/>
 </head>
+
 <body>
-<div class="container">
 <nav>
-    <div class="nav-wrapper purple darken-4">
-    <a href="/" class="brand-logo center">PHP</a>
+    <div class="nav-wrapper  darken-4">
+    <a href="/" class="brand-logo center"><img src="/img/php.png"/></a>
       <ul id="nav-mobile" class="left hide-on-med-and-down">
       <?php foreach( [
-			'/basics' => 'Основи',
-			'/layout' => 'Шаблонізація' 
-		    ] as $href => $name ) : ?>
-            
-			<li <?= $uri==$href ? 'class="active"' : '' ?> ><a href="<?= $href ?>"><?= $name ?></a></li>
+			'basics' => 'Основи',
+			'layout' => 'Шаблонізація',
+      'api' => 'API', 
+		    ] as $href => $name ) : ?>            
+			<li <?= $uri==$href ? 'class="active"' : '' ?> ><a href="/<?= $href ?>"><?= $name ?></a></li>
 		<?php endforeach ?>
       </ul>
     </div>
   </nav>
 
+<div class="container">
   <?php include $page_body ; ?>
 </div>
 
    <!-- Compiled and minified JavaScript -->
    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-</body>
+   <script src="/js/site.js"></scrypt>
+  </body>
 </html>   
---
+++ controllers/TestController.php
@@ -0,0 +1,68 @@
+<?php
+
+class TestController {
+	
+	public function serve() {
+		$method = strtolower( $_SERVER[ 'REQUEST_METHOD' ] ) ;   // метод запиту - GET, POST, ...
+		$action = "do_{$method}" ;
+		// чи визначений у даному об'єкті метод з іменем $action (do_get)
+		if( method_exists( $this, $action ) ) {
+			// якщо визначений, то викликаємо
+			$this->$action() ;   // у РНР $this-> обов'язково
+			// !! назву методу можна передати через змінну
+			// $this->$action()  == $this->do_get()
+		}
+		else {
+			http_response_code( 405 ) ;
+			echo "Method Not Allowed" ;
+		}
+	}
+	
+	protected function connect_db_or_exit() { // Встановлює з'єднання з базою даних MySQL за допомогою PDO
+		try {
+			return new PDO(
+				'mysql:host=localhost;dbname=php_spd_111;charset=utf8mb4', 
+				'spd_111_user', 'spd_pass', [
+					PDO::ATTR_PERSISTENT => true,
+					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,					
+			] ) ;
+		} 
+		catch( PDOException $ex ) {
+			http_response_code( 500 ) ;
+			echo "Connection error: " . $ex->getMessage() . $ex->getMessage() . $ex->getCode() . var_dump($ex);
+			exit ;
+		}
+		
+	}
+
+	// do_get() метод:
+	// Викликає connect_db_or_exit() для отримання об'єкта PDO та встановлення 
+	// з'єднання з базою даних
+	// Виконує SQL-запит для створення таблиці Users у випадку, якщо вона ще не існує.
+	protected function do_get() {   
+		$db = $this->connect_db_or_exit() ;
+		// виконання запитів
+		$sql = "CREATE TABLE  IF NOT EXISTS  Users (
+			`id`        CHAR(36)      PRIMARY KEY  DEFAULT ( UUID() ),
+			`email`     VARCHAR(128)  NOT NULL,
+			`name`      VARCHAR(64)   NOT NULL,
+			`password`  CHAR(32)      NOT NULL     COMMENT 'Hash of password',
+			`avatar`    VARCHAR(128)  NULL
+		) ENGINE = INNODB, DEFAULT CHARSET = utf8mb4";
+		try {
+			$db->query( $sql ) ;
+		}
+		catch( PDOException $ex ) {
+			http_response_code( 500 ) ;
+			echo "query error: " . $ex->getMessage() ;
+			exit ;
+		}
+		echo "Hello from do_get - Query OK" ;
+	}
+	
+	// do_post() метод:
+    // Просто виводить повідомлення "Hello from do_post".
+	protected function do_post() {
+		echo "Hello from do_post" ;
+	}
+}
