9f5ff014b7f3646fc6d3440578e9ded378b3c645
Yurii-ua74 <postffild@gmail.com>
2024-02-18 09:26:31 +0200

    api2

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -1,40 +1,8 @@
 <?php
 
-class TestController {
-	
-	public function serve() {
-		$method = strtolower( $_SERVER[ 'REQUEST_METHOD' ] ) ;   // метод запиту - GET, POST, ...
-		$action = "do_{$method}" ;
-		// чи визначений у даному об'єкті метод з іменем $action (do_get)
-		if( method_exists( $this, $action ) ) {
-			// якщо визначений, то викликаємо
-			$this->$action() ;   // у РНР $this-> обов'язково
-			// !! назву методу можна передати через змінну
-			// $this->$action()  == $this->do_get()
-		}
-		else {
-			http_response_code( 405 ) ;
-			echo "Method Not Allowed" ;
-		}
-	}
-	
-	public function connect_db_or_exit() { // Встановлює з'єднання з базою даних MySQL за допомогою PDO
-		try {
-			return new PDO(
-				'mysql:host=localhost;dbname=php_spd_111;charset=utf8mb4', 
-				'spd_111_user', 'spd_pass', [
-					PDO::ATTR_PERSISTENT => true,
-					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,					
-			] ) ;
-		} 
-		catch( PDOException $ex ) {
-			http_response_code( 500 ) ;
-			echo "Connection error: " . $ex->getMessage() . $ex->getMessage() . $ex->getCode() . var_dump($ex);
-			exit ;
-		}
-		
-	}
+include_once "ApiController.php";
 
+class TestController extends ApiController {
 	// do_get() метод:
 	// Викликає connect_db_or_exit() для отримання об'єкта PDO та встановлення 
 	// з'єднання з базою даних
